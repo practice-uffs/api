5ec9f12c525df08c1f30da4bafb20ea34bd01006
Raphael Borges <rah-borges@hotmail.com>
2021-02-20 01:15:44 -0300

    feart(auth idUFFS):add api do cc_url idUFFS

--- config/app.php
+++ config/app.php
@@ -43,6 +43,19 @@ return [
 
     /*
     |--------------------------------------------------------------------------
+    | API CC
+    |--------------------------------------------------------------------------
+    |
+    | Configurações relacionadas à API-CC.
+    |
+    */
+
+    'apicc_url' => env('APICC_URL', 'http://localhost'),
+    'apicc_token' => env('APICC_TOKEN', ''),
+
+
+    /*
+    |--------------------------------------------------------------------------
     | Application URL
     |--------------------------------------------------------------------------
     |
@@ -80,7 +93,7 @@ return [
     |
     */
 
-    'locale' => 'en',
+    'locale' => 'pt',
 
     /*
     |--------------------------------------------------------------------------
@@ -93,7 +106,7 @@ return [
     |
     */
 
-    'fallback_locale' => 'en',
+    'fallback_locale' => 'pt',
 
     /*
     |--------------------------------------------------------------------------
@@ -106,7 +119,7 @@ return [
     |
     */
 
-    'faker_locale' => 'en_US',
+    'faker_locale' => 'pt_BR',
 
     /*
     |--------------------------------------------------------------------------
@@ -216,7 +229,7 @@ return [
         'Password' => Illuminate\Support\Facades\Password::class,
         'Queue' => Illuminate\Support\Facades\Queue::class,
         'Redirect' => Illuminate\Support\Facades\Redirect::class,
-        // 'Redis' => Illuminate\Support\Facades\Redis::class,
+        'Redis' => Illuminate\Support\Facades\Redis::class,
         'Request' => Illuminate\Support\Facades\Request::class,
         'Response' => Illuminate\Support\Facades\Response::class,
         'Route' => Illuminate\Support\Facades\Route::class,
