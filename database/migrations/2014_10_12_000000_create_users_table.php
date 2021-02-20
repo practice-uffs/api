8492d3f85f448002ffc51b46915611925d31f8a6
Raphael Borges <rah-borges@hotmail.com>
2021-02-20 00:07:28 -0300

    feat(db):criado migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,16 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->string('username');
+            $table->string('uid')->unique();
+            $table->string('cpf')->nullable();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('email');
+            $table->string('type')->nullable();
+            $table->text('password')->nullable(false);
             $table->rememberToken();
+            $table->timestamp('email_verified_at')->nullable();
             $table->timestamps();
         });
     }
