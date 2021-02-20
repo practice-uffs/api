45f8aca1fb9852192a6bb3708a2d2510f22397a7
Raphael Borges <rah-borges@hotmail.com>
2021-02-20 00:02:44 -0300

    refact(routes):rm login das que necessitam auth

--- routes/api.php
+++ routes/api.php
@@ -18,8 +18,9 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('login', 'API\AuthController@login');
+
 Route::group(['middleware' => 'api','prefix' => 'auth'], function ($router) {
-    Route::post('login', 'API\AuthController@login');
     Route::post('logout', 'API\AuthController@logout');
     Route::post('refresh', 'API\AuthController@refresh');
     Route::post('me', 'API\AuthController@me');
