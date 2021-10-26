e6ce4c0692ff358635862af5a182c70363e27569
Alisson <alisson.luan2000@gmail.com>
2021-10-26 10:08:24 -0300

    fix error connection

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -33,7 +33,7 @@ return [
         'pusher' => [
             'driver' => 'pusher',
             'key' => env('PUSHER_APP_KEY'),
-            'secret' => null,
+            'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
