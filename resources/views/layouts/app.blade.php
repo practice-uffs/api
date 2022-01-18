bdfe7744e2e9abdcd2be3877f1f3e60104b6cda3
Guilherme Graeff <guilherme.rafael.graeff@gmail.com>
2022-01-18 13:52:39 -0300

    Add Chat screen style

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,37 +9,38 @@
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
         <!-- Styles -->
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-        <link rel="stylesheet" href="{{ mix('css/aura/aura.css') }}">
+
+        <link type="text/css" rel="stylesheet" href="{{asset('/css/aura.css')}}">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
         @livewireStyles
-        @bukStyles(true)
 
         <!-- Scripts -->
         <script src="{{ mix('js/app.js') }}" defer></script>
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-ccuffs">
-            @livewire('navigation-dropdown')
+        <div class="min-h-screen bg-ccuffs h-100">
 
             <!-- Page Heading -->
             <header class="bg-ccuffs-light shadow">
                 <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
+                    {{ $header ?? '' }}
                 </div>
             </header>
 
             <!-- Page Content -->
-            <main>
+            <main class="h-100">
                 {{ $slot }}
             </main>
         </div>
 
         @stack('modals')
-
+        
         @livewireScripts
-        @bukScripts(true)
+        <!-- @bukScripts(true) -->
     </body>
 </html>
