0ab32f1ad522a779190f70d6548c2bed88b1bd39
Stefani Meneghetti <meneghettistefani@gmail.com>
2022-07-20 10:24:20 -0300

    feat: AcademicCalendarSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            AppSeeder::class
+            AppSeeder::class,
+            AcademicCalendarSeeder::class
         ]);
     }
 }
