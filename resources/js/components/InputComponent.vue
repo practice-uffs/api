2fd5c81c926533e47f185dc81a319662ceb0786f
Guilherme <guilherme.devon@hotmail.com>
2022-08-25 23:42:03 -0300

    Chores: Efeito de Fade no header

--- resources/js/components/InputComponent.vue
+++ resources/js/components/InputComponent.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="input-bar">
         <div class="input-group">
-            <input v-model="inputMessage" name="message" @keyup.enter="sendMessage" type="text" @click="changeHeaderDisplay()" class="form-control text-input" placeholder="O que deseja saber?" />
+            <input v-model="inputMessage" name="message" @keyup.enter="sendMessage" type="text" class="form-control text-input" placeholder="O que deseja saber?" @focus="changeHeaderDisplay()" @blur="changeHeaderDisplay()"/>
             <a class="input-group-text send-button" @click="sendMessage"><img src="/img/aura/sendIcon.png" class="send-icon"></a>
         </div>
     </div>
@@ -18,7 +18,8 @@ export default {
     },
     methods: {
         changeHeaderDisplay() {
-            this.$emit('update:showheader', !this.showheader);
+            if (window.innerWidth < 449)
+              this.$emit('update:showheader', !this.showheader);
         },  
 
         sendMessage() {
